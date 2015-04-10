0cf1cba74ab31a18beb86916ad35dc6151a860fe
tomanmil <tomanmil@stanford.edu>
2015-04-09 22:56:49 -0700

    opener
    project!

---
+++ opener-project/fetch-and-unpack-twitter-data.py
@@ -0,0 +1,26 @@
+import requests
+import shutil
+import os.path
+from glob import glob
+from tempfile import NamedTemporaryFile
+
+DATA_URL = 'http://stash.compjour.org/data/sunlight-twitter-opener.zip'
+DATA_DIR = 'data-hold'
+
+tzip = NamedTemporaryFile(suffix = '.zip')
+
+print("Downloading", DATA_URL)
+r = requests.get(DATA_URL)
+tzip.write(r.content)
+
+tzip.seek(0)
+print("Unzipping to", DATA_DIR)
+shutil.unpack_archive(tzip.name, DATA_DIR)
+
+tzip.close()
+
+csvname = os.path.join(DATA_DIR, 'sunlight_legislators.csv')
+print(csvname, 'has', os.path.getsize(csvname), 'bytes')
+print("Tweet files:", len(glob('data-hold/tweets/*.json')))
+print("Profile files:", len(glob('data-hold/profiles/*.json')))
+        
---
+++ opener-project/hello-world.py
@@ -0,0 +1,7 @@
+print("Hello world")
+myname = "Dan"
+print("My name is", myname)
+myage = 42
+print("I am", myage, "years old")
+print("That is roughly", 42 * 365, "days")
+print("Or,", 42 * 365 * 24 * 60 * 60, "seconds" )
