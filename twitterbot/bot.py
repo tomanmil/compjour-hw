c23c14ccfff4163181e4bd9842c98f36dbbc74cc
tomanmil <tomanmil@stanford.edu>
2015-04-26 14:51:58 -0700

    twitterbot
    skeleton

---
+++ twitterbot/bot.py
@@ -0,0 +1,10 @@
+import twit_utils
+import foo
+CREDS_FILE = "~/Desktop/tw.json"
+
+def make_it_so():
+	#step 1: use search/tweets endpoint to find tweets with 'different than'
+	#step 2: store screen name/user ID
+	#step 3: check user's timeline for other tweets with 'different than'
+	#step 4: compose tweet text 
+	#step 5: send tweet
---
+++ twitterbot/foo.py
@@ -0,0 +1,22 @@
+def count_diff_than(tweets):
+	"""
+	Given the list of tweets, from the user_timeline API endpoint 
+	Count the number of statuses that contain the phrase different than
+
+	Arguments:
+		a list of twitter tweet objects that are dicts
+	Returns:
+		A count of the number of statuses using "different than"
+	"""
+
+def custom_message(count):
+	"""
+	Create a custom reply to a user who uses different than
+	Arguments:
+		Count the number of tweets containing different than
+	Returns: 
+		A text string containing a message, the number of 'different than' tweets
+		and a url to a page explaining the difference
+	"""
+
+	
