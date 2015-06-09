9ac6c577bb638c780c442e9c88e1f687eeea6d9d
tomanmil <tomanmil@stanford.edu>
2015-06-08 18:39:41 -0700

    94.py
    scraper

--- search-script-scrape/94.py
+++ search-script-scrape/94.py
@@ -1,12 +1,19 @@
 import csv
 from operator import itemgetter
 import requests
+
+def cleanscore(score):
+	if score == 's':
+		return 0
+	return int(score)
+
 CSVURL = 'https://data.cityofnewyork.us/api/views/f9bf-2cp4/rows.csv?accessType=DOWNLOAD'
 response = requests.get(CSVURL)
 f = open("SAT.csv", "w")
-f.write(response.text)
+f.write(response.text.encode('ascii', 'ignore').decode('ascii'))
 f.close()
 
 data = csv.DictReader(open("SAT.csv"))
 rows = list(data)
-sorted(rows,key=itemgetter('SAT Math Avg. Score'))[0]['SCHOOL NAME']
+newrows = [[row['SCHOOL NAME'],cleanscore(row['SAT Math Avg. Score'])] for row in rows]
+print(sorted(newrows,key=itemgetter(1),reverse=True)[0][0])
