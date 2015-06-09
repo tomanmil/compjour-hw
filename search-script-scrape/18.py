b3beff92819a197ff4250fd2e053ff48a8c7b034
tomanmil <tomanmil@stanford.edu>
2015-06-08 21:13:40 -0700

    18.py
    scraper

---
+++ search-script-scrape/18.py
@@ -0,0 +1,10 @@
+import csv
+import requests
+CSVURL = 'http://unitedstates.sunlightfoundation.com/legislators/legislators.csv'
+response = requests.get(CSVURL)
+f = open("legislators.csv", "w")
+f.write(response.text.encode('ascii', 'ignore').decode('ascii'))
+f.close()
+data = csv.DictReader(open("legislators.csv"))
+rows = list(data)
+print(len([i for i in rows if i['twitter_id'] and i['in_office'] == '1']))
